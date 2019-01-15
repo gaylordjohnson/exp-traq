3dea582e83d70815fe7463792690ee62ebb8481e
Gaylord Johnson <gaylordjohnson@users.noreply.github.com>
2019-01-14 20:08:03 -0500

    Finished changing edit form from inline to modal. Uses Bootstrap modal. Seems to be working so far.

--- js/main.js
+++ js/main.js
@@ -115,45 +115,44 @@ function showChangeTraq() {
   document.getElementById('changeExpTraqField').focus();
 }
 
-var entryBeingEdited = null; // Entry currently being edited
+// Global... Maybe there's a better way to pass this, but this was easiest...
+var idOfEntryBeingEdited; 
 
-// Called when user clicks Edit on an entry
-function editEntry(id) {
-  if (entryBeingEdited)
-    entryBeingEdited.style.display = 'block'; // Un-hide entry previously being edited
-  entryBeingEdited = document.getElementById(id);
-  form = document.getElementById('editForm');
-
-  // Using some jQuery here, since I have jQuery included anyway (required by Bootstrap)
-  const amount = $('#' + id).find('.amount').text();
-  const payee = $('#' + id).find('.payee').text();
-  const date = $('#' + id).find('.dateYMD').text();
-  const comment = $('#' + id).find('.comment').text();
-  console.log('Editing: [' + date + '] [' + amount + '] [' + payee + '] [' + comment + ']');
-
-  // Assign amount etc. as values inside the form
-  $(form).find("[name='amount']").attr('value', amount);
-  $(form).find("[name='payee']").attr('value', payee);
-  $(form).find("[name='date']").attr('value', date);
-  $(form).find("[name='comment']").val(comment);
-
-  entryBeingEdited.parentNode.insertBefore(form, entryBeingEdited);
-  entryBeingEdited.style.display = 'none';
-  form.style.display = 'block';
-
-  //xx DOESN'T WORK :-(
-  // I debugged it and what happens is it gets scrolled into view but then
-  // something causes the page to instantly scroll to the top again.
-  // Can't figure out what it is...
-  form.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-}
-
-// Once page loads, add 'submit' event listener to the editForm.
-// (If page not loaded, there's nothing to add listener to.)
-// Listener is called when user clicks Save on the entry's edit form.
+// Wrapping the below code in window.addEventListener because this code will only work
+// *after* jQuery lib has been loaded, and it gets loaded right before the </body>
+// Otherwise it gives error "variable $ not found". Also, editForm needs to have been loaded.
 window.addEventListener('load', function() {
-  // Check for presence of form because in our **table** view we have no form
-  if (document.getElementById('editForm')) document.getElementById('editForm').addEventListener('submit', function(event) {
+  //
+  // Adding hook #1: handling user clicking edit, to populate the form with values
+  //
+  // From https://getbootstrap.com/docs/4.0/components/modal/
+  // This is how we instantiate the modal's content from the triggering entry
+  $('#editModal').on('show.bs.modal', function (event) {
+    var triggerer = $(event.relatedTarget); // Button that triggered the modal
+    idOfEntryBeingEdited = triggerer.data('entry-id'); // Extract entry id from triggerer's data-entry-id attribute
+
+    // Now that we have entry id, we use some jQuery to get amount, payee, etc.
+    // (recall that jQuery is required by Boostrap, so we get it for free)
+    const amount = $('#' + idOfEntryBeingEdited).find('.amount').text();
+    const payee = $('#' + idOfEntryBeingEdited).find('.payee').text();
+    const date = $('#' + idOfEntryBeingEdited).find('.dateYMD').text();
+    const comment = $('#' + idOfEntryBeingEdited).find('.comment').text();
+    console.log('Editing: [' + date + '] [' + amount + '] [' + payee + '] [' + comment + ']');
+    
+    // Update the modal's content. We'll use jQuery here, but could use a data binding library or other methods instead.
+    var modal = $(this)
+    // Assign amount etc. as values inside the form
+    modal.find("[name='amount']").attr('value', amount);
+    modal.find("[name='payee']").attr('value', payee);
+    modal.find("[name='date']").attr('value', date);
+    modal.find("[name='comment']").val(comment);
+    //modal.find('.modal-title').text('Editing #' + id)
+  });
+
+  //
+  // Adding hook #2: intercept edit form submission and handle it OUR way
+  //
+  document.getElementById('editForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevents form submission; but field validation still takes place
 
     var ajaxRequest = new XMLHttpRequest();
@@ -163,7 +162,7 @@ window.addEventListener('load', function() {
     ajaxRequest.onreadystatechange = function() {
       if (ajaxRequest.readyState == 4 && ajaxRequest.status == 200) {
         console.log('Edit request completed successfully; reloading page');
-        window.location.reload() //xx Not sure if I need to reload here, or from python
+        window.location.reload() // Wasn't sure if I need to reload here, or from python...
       }
     };
 
@@ -175,8 +174,8 @@ window.addEventListener('load', function() {
     const comment = formData.get('comment');
     console.log('Will PUT [' + date + '] [' + amount + '] [' + payee + '] [' + comment + ']');
 
-    ajaxRequest.open('PUT', 'entry/' + entryBeingEdited.id);
-    // From my debugging, seems like webapp2 expects form parameters as application/x-www-form-urlencoded
+    ajaxRequest.open('PUT', 'entry/' + idOfEntryBeingEdited);
+    // From my debugging, turns out webapp2 expects form parameters as application/x-www-form-urlencoded
     ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     var contentBody = "";
     contentBody += 'amount=' + encodeURIComponent(amount) + '&';
@@ -185,22 +184,11 @@ window.addEventListener('load', function() {
     contentBody += 'comment=' + encodeURIComponent(comment);
     ajaxRequest.send(contentBody); 
   
-    // Close form; show entry: this is same as if user clicked Cancel
-    closeEditForm();
-
     // Not sure this is needed. Seems to work w/o it as well but 
     // some on Stackoverflow say need to return false to prevent default behavior
     return false; 
   });
 });
-
-// Used in two places: when clicking Cancel, and at the end of Edit
-function closeEditForm() {
-  form = document.getElementById('editForm');
-  form.style.display = 'none';
-  entryBeingEdited.style.display = 'block';
-  entryBeingEdited.scrollIntoView(); //xx DOESN'T WORK
-}
 
 function deleteEntry(id) {
   var okToDelete = confirm("OK to delete entry?");
