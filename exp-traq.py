28a8dc88119f58c78bb09e8b28e904313f31d358
Gaylord Johnson <gaylordjohnson@users.noreply.github.com>
2022-08-05 19:43:31 -0400

    Removed unused 'import locale'

--- exp-traq.py
+++ exp-traq.py
@@ -7,7 +7,6 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 import jinja2
 import webapp2
-import locale
 
 JINJA_ENVIRONMENT = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
